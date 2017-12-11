41e1dab0c8a4e6f6e696f330bcc146d28f5d9230
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-11 12:33:13 -0500

    Tecumseh Mall 1.1  /helper.js

--- helper.js
+++ helper.js
@@ -77,8 +77,8 @@ function init() {
         });
     });
     //dynamically changing copyright year
-var current_year = moment().year();
-$("#current_year").text(current_year);
+    var current_year = moment().year();
+    $("#current_year").text(current_year);
 }
 
 function show_content(){
